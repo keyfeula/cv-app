0874f6612dcef397ffa6853c67cd19e0ba3ca92b
Key Feula <keyfeula1214@gmail.com>
2025-10-15 23:10:14 -0700

    Add component for cv controls and display and styling for both

---
+++ src/components/CVControl.jsx
@@ -0,0 +1,9 @@
+import "../styles/CVControl.css"
+
+export function CVControl(props) {
+    return (
+        <div className="cv-control">
+            <h1>Component Title</h1>
+        </div>
+    )
+}
---
+++ src/components/CVDisplay.jsx
@@ -0,0 +1,9 @@
+import "../styles/CVDisplay.css"
+
+export function CVDisplay(props) {
+    return (
+        <div className="display">
+            TEST DISPLAY
+        </div>
+    )
+}
